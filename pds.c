f5e9c37f296013b6023452e17d157a777a32bbcd
ANDREI2707 <61541965+ANDREI2707@users.noreply.github.com>
2022-05-03 21:12:29 +0300

    Add files via upload
    up2

--- pds.c
+++ pds.c
@@ -63,14 +63,38 @@ int main() {
 
 	fwrite(&header, sizeof(header), 1, file_out);
 	fwrite(&chunk, sizeof(chunk), 1, file_out);
-
+	int32_t sample_buffer_L[6] = { 0, 0, 0, 0, 0, 0 };
+	int32_t sample_buffer_R[6] = { 0, 0, 0, 0, 0, 0 };
 	int16_t s_buf[2*SIZE];
+	int16_t outputL;
+	int16_t outputR;
+	int16_t signalL;
+	int16_t signal;
+
+	int a0, a1, a2, b1, b2;
+	int32_t coeffs[5] = { 0, 0, 0, 0, 0 };
+
+	a0 = 0.2185718974189478;
+	a1 = 0.2754975197979510;
+	a2 = 0.2145185794171419;
+	b1 = 0.1115854841587418;
+	b2 = 0.1958794741897444;
+
+	coeffs[0] = flt2fixd(a0);
+	coeffs[1] = flt2fixd(a1);
+	coeffs[2] = flt2fixd(a2);
+	coeffs[3] = flt2fixd(b1);
+	coeffs[4] = flt2fixd(b2);
 	while (1)
 	{
 		size_t read_size = fread(s_buf, 2*sample_size, 128, file_in);
-		for (int n = 0; n < sample_size; n++) {
-			output =  IIR(sample_buffer)
+		for (int n = 0; n < read_size; n++) {
 
+			outputL = IIR(sample_buffer_L, coeffs,s_buf[2*n]);
+			outputR = IIR(sample_buffer_R, coeffs, s_buf[2 * n + 1]);
+
+			s_buf[2*n] = outputL;
+			s_buf[2 * n + 1] = outputR;
 		}
 	....
 	}
